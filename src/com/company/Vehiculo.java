687460d5fb6bd05c84fafbb4e04bc5aab2daa533
Edu <eduardo@poincenot.com>
2020-03-19 18:46:41 -0300

    Parte A Ejercicio Dakar

---
+++ src/com/company/Vehiculo.java
@@ -0,0 +1,74 @@
+package com.company;
+
+
+public class Vehiculo {
+
+    private Integer velocidad;
+    private Integer aceleracion;
+    private Integer anguloDeGiro;
+    private String patente;
+    private Double peso;
+    private Integer rueda;
+
+    public Vehiculo(Integer velocidad, Integer aceleracion, Integer anguloDeGiro, String patente, Double peso, Integer rueda) {
+        this.velocidad = velocidad;
+        this.aceleracion = aceleracion;
+        this.anguloDeGiro = anguloDeGiro;
+        this.patente = patente;
+        this.peso = peso;
+        this.rueda = rueda;
+    }
+
+    public Integer getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(Integer velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public Integer getAceleracion() {
+        return aceleracion;
+    }
+
+    public void setAceleracion(Integer aceleracion) {
+        this.aceleracion = aceleracion;
+    }
+
+    public Integer getAnguloDeGiro() {
+        return anguloDeGiro;
+    }
+
+    public void setAnguloDeGiro(Integer anguloDeGiro) {
+        this.anguloDeGiro = anguloDeGiro;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Integer getRueda() {
+        return rueda;
+    }
+
+    public void setRueda(Integer rueda) {
+        this.rueda = rueda;
+    }
+
+
+
+
+
+}
