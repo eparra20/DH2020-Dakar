317002c481c90dd003fe694062cfbf3841a57660
Edu <eduardo@poincenot.com>
2020-03-19 18:57:15 -0300

    Punto 2 : Modelar la clase Carrera

---
+++ src/com/company/Carrera.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Carrera {
+
+    private Integer distancia;
+    private Double premioEnDolares;
+    private String nombre;
+    private Integer cantidadDeVehiculosPermitidos;
+    private List<Vehiculo> listaDeVehiculos; //una Lista o Conjunto (Set)
+
+    public Carrera(Integer distancia, Double premioEnDolares, String nombre, Integer cantidadDeVehiculosPermitidos) {
+        this.distancia = distancia;
+        this.premioEnDolares = premioEnDolares;
+        this.nombre = nombre;
+        this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
+        this.listaDeVehiculos = new ArrayList<>();
+    }
+
+    public Integer getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(Integer distancia) {
+        this.distancia = distancia;
+    }
+
+    public Double getPremioEnDolares() {
+        return premioEnDolares;
+    }
+
+    public void setPremioEnDolares(Double premioEnDolares) {
+        this.premioEnDolares = premioEnDolares;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidadDeVehiculosPermitidos() {
+        return cantidadDeVehiculosPermitidos;
+    }
+
+    public void setCantidadDeVehiculosPermitidos(Integer cantidadDeVehiculosPermitidos) {
+        this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
+    }
+
+    public List<Vehiculo> getListaDeVehiculos() {
+        return listaDeVehiculos;
+    }
+
+    public void setListaDeVehiculos(List<Vehiculo> listaDeVehiculos) {
+        this.listaDeVehiculos = listaDeVehiculos;
+    }
+}
